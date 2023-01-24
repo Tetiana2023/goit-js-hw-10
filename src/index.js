4a5f580b94bb3fdf4f5e7092600a3caa1e87e1c9
Tetiana2023 <drofaj18@gmail.com>
2023-01-24 11:53:36 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.getElementById('search-box');
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+inputEl.addEventListener('input', debounce((findCountry), DEBOUNCE_DELAY));
