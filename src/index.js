ae4f9bebc913caec5e4f32784f6d66a2e3d34642
Tetiana2023 <drofaj18@gmail.com>
2023-01-24 21:17:34 +0200

    task -01

--- src/index.js
+++ src/index.js
@@ -7,4 +7,28 @@ const inputEl = document.getElementById('search-box');
 const countryList = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
 
-inputEl.addEventListener('input', debounce((findCountry), DEBOUNCE_DELAY));
+console.log(inputEl);
+
+//inputEl.addEventListener('input', debounce((findCountry), DEBOUNCE_DELAY));
+
+//  fetch(`https://restcountries.com/v3.1/all`)
+//  .then(response => {
+//     return response.json();
+// })
+// .then(country => {console.log(country);
+// })
+// .catch(error => {console.log(error);
+// })
+   
+   
+//   function findCountry() {
+ fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+   .then(response => {
+    return response.json()
+}).then(countries => {console.log(countries)
+    }).catch(error => {
+        console.log(error)
+    });
+  
+
+
